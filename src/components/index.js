1f6c73f3c917f9d822eeec2bb032fadbe08e5a93
Hunaid Baig <Hunaidbaig@gmail.com>
2021-12-26 21:10:52 +0500

    firt commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Articles} from './articles/Articles';
+export {default as Brand} from './brand/Brand';
+export {default as CTA} from './cta/CTA';
+export {default as Feature} from './feature/Features';
+export {default as NavBar} from './navbar/NavBar';
